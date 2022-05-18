0651c4c2a103d9d56b24f31ad9a5fcf3680961fe
SoHenriquez <estebansotohenriquez@gmail.com>
2022-05-17 22:57:08 -0400

    Conexion a base de datos

---
+++ bds/conexion.php
@@ -0,0 +1,20 @@
+<?php 
+/* Datos de conexion a la base de datos */
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "avisos";
+
+$con = mysqli_connect($db_host, $db_user,$db_pass,$db_name);
+
+if(!$con){
+
+    echo 'No se puede conectar a la base de datos: ' .mysqli_connect_errno()();
+
+}else{
+    echo 'Conectadosky';
+}
+
+
+?>
