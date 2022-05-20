6bbb9396308d1be147b00461cb7e4a3d9e1ee3c8
SoHenriquez <estebansotohenriquez@gmail.com>
2022-05-19 23:40:19 -0400

    Boceto de Tabla Avisos Destacados

---
+++ partes/hu1_aviso/avisos_destacados.php
@@ -0,0 +1,35 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col-1">#</th>
+      <th scope="col-1">Nombre</th>
+      <th scope="col-1">Correo</th>
+      <th scope="col-1">Titulo</th>
+      <th scope="col-1">Tipo</th>
+      <th scope="col-1">Fecha</th>
+      <th scope="col-1">Hora</th>
+      <th scope="col-1">Destacado</th>
+      <th scope="col-3">Descripcion</th>
+      <th scope="col-1">Accion</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
